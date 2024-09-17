23a9945e24ec53c1e4126766298034262c2939fc
IMoronPirate <i.moron.pirate@gmail.com>
2024-09-17 15:10:09 +0200

    change name of enum

--- src/strain.rs
+++ src/strain.rs
@@ -1,5 +1,6 @@
+/// All possible outcome of experiment
 #[derive(Clone)]
-pub enum ProbOutcome {
+pub enum State {
     S(f64),
     I(f64),
     R(f64),
@@ -7,25 +8,25 @@ pub enum ProbOutcome {
     TechnicalError(f64),
 }
 
-impl ProbOutcome {
+impl State {
     pub(crate) fn value(&self) -> f64 {
         match &self {
-            ProbOutcome::S(x) => *x,
-            ProbOutcome::I(x) => *x,
-            ProbOutcome::R(x) => *x,
-            ProbOutcome::Inconclusive(x) => *x,
-            ProbOutcome::TechnicalError(x) => *x,
+            State::S(x) => *x,
+            State::I(x) => *x,
+            State::R(x) => *x,
+            State::Inconclusive(x) => *x,
+            State::TechnicalError(x) => *x,
         }
     }
 }
 
 pub struct Strain {
-    p: Vec<ProbOutcome>,
+    p: Vec<State>,
 }
 
 impl Strain {
 		/// Generate a new Strain check for consistency in input probabilities
-    pub fn new(p: &[ProbOutcome; 5]) -> Self {
+    pub fn new(p: &[State; 5]) -> Self {
         assert_eq!(
             p.iter().map(|x| x.value()).sum::<f64>(),
             1.0,
@@ -35,11 +36,11 @@ impl Strain {
         let mut tot_states = 0;
         for state in p {
             match state {
-                ProbOutcome::S(_) => tot_states += 1,
-                ProbOutcome::I(_) => tot_states += 10,
-                ProbOutcome::R(_) => tot_states += 100,
-                ProbOutcome::Inconclusive(_) => tot_states += 1000,
-                ProbOutcome::TechnicalError(_) => tot_states += 10_000,
+                State::S(_) => tot_states += 1,
+                State::I(_) => tot_states += 10,
+                State::R(_) => tot_states += 100,
+                State::Inconclusive(_) => tot_states += 1_000,
+                State::TechnicalError(_) => tot_states += 10_000,
             }
         }
         assert_eq!(tot_states, 11111, "Missing one possible state outcome.");
@@ -55,11 +56,11 @@ mod tests {
     #[should_panic(expected = "Sum of probability not 1.")]
     fn check_if_greater_then_1_prob() {
         Strain::new(&[
-            ProbOutcome::S(0.9),
-            ProbOutcome::R(0.1),
-            ProbOutcome::I(0.5),
-            ProbOutcome::Inconclusive(0.),
-            ProbOutcome::TechnicalError(0.),
+            State::S(0.9),
+            State::R(0.1),
+            State::I(0.5),
+            State::Inconclusive(0.),
+            State::TechnicalError(0.),
         ]);
     }
 
@@ -67,11 +68,11 @@ mod tests {
     #[should_panic(expected = "Negative probability.")]
     fn check_if_less_than_zero_prob() {
         Strain::new(&[
-            ProbOutcome::S(0.9),
-            ProbOutcome::R(0.2),
-            ProbOutcome::I(-0.1),
-            ProbOutcome::Inconclusive(0.),
-            ProbOutcome::TechnicalError(0.),
+            State::S(0.9),
+            State::R(0.2),
+            State::I(-0.1),
+            State::Inconclusive(0.),
+            State::TechnicalError(0.),
         ]);
     }
 
@@ -79,11 +80,11 @@ mod tests {
     #[should_panic(expected = "Missing one possible state outcome.")]
     fn check_if_missing_state() {
         Strain::new(&[
-            ProbOutcome::S(0.9),
-            ProbOutcome::R(0.1),
-            ProbOutcome::R(0.0),
-            ProbOutcome::Inconclusive(0.),
-            ProbOutcome::TechnicalError(0.),
+            State::S(0.9),
+            State::R(0.1),
+            State::R(0.0),
+            State::Inconclusive(0.),
+            State::TechnicalError(0.),
         ]);
     }
 }
